78b815d0140ad5a363fb2fc186edb1e6fe5f1e03
ange Ntamack <angentamack@MacBook-Pro-de-ange.local>
2021-10-01 12:12:33 +0200

    authentification user Done

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -7,6 +7,7 @@
 
 <script>
 import FormUser from "../components/FormUser.vue";
+import axios from "axios";
 
 export default{
     name: "Login",
@@ -17,7 +18,19 @@ export default{
         submit (value) {
 
             let userData = Object.assign({}, value)
-            console.log("this is login", userData)
+            // console.log("this is login", userData)
+
+            axios.post("http://localhost:5500/user/login", userData)
+            .then(response => {
+                // console.log(response.data.result[0]['id']);
+                localStorage.setItem('id', response.data.result[0]['id'])
+                if(response.status === 200){
+                    this.$router.push({name: "Home"})
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            })
 
         }
     }
--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const cors = require('cors')
+const express    = require('express');
+const cors       = require('cors')
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const bcrypt = require('bcrypt');
+const mysql      = require('mysql');
+const session    = require('session');
+const bcrypt     = require('bcrypt');
 
-const axios = require('axios');
+const axios      = require('axios');
 
 
 const app = express();
@@ -16,8 +17,8 @@ app.use(express.json());
 // CREATE TABLE user (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, username VARCHAR(255), email VARCHAR(255) UNIQUE NOT NULL, password VARCHAR(255));
 
 const db = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
+    user    : 'root',
+    host    : 'localhost',
     password: 'root',
     database: 'pixilive'
 
@@ -32,33 +33,43 @@ app.get('/', function (req, res) {
     res.send("HELLO WORLD")
 })
 
-app.post('/user/register', function (req, res) {
-    // const {username, email, password} = req.body
-    
+app.post('/user/register', async(req, res) => {
+    const salt = await bcrypt.genSalt(10)
+    const hashPass = await bcrypt.hash(req.body.password,salt); 
+
     const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
+    const email    = req.body.email;
+    const password = hashPass;
     
     console.log(req.body);
-    db.query("INSERT INTO user (username, email, password) VALUES(?, ?, ?)", 
+    db.query("INSERT INTO user (username, email, password) VALUES (?, ?, ?)", 
     [username, email, password],
     (err, result) => {
-        if (err) {
-            console.log(err, "this email is already in use")
-            res.status(400).send({error: "Sign up failed"})
+        if (err){
+            throw(err)
+        }else{
+            res.send({result, msg: "you ar signed"})
         }
-    
-        res.send({result, msg: "you ar signed"})
 
     })
 })
 
-app.post('/user/login', function (req, res) {
-    db.query('INSERT INTO user VALUES (?, ?, ?)', [],
-         (err, result) => {
-             
-         });
-    res.send("HELLO WORLD")
+app.post('/user/login', (req, res) => {
+
+    const email    = req.body.email;
+    const password = req.body.password;
+
+    console.log(req.body);
+    db.query("SELECT * FROM user WHERE email = ?;", [email],
+    (err, result, fields) => {
+        if(err) throw err;
+
+        if(result.length > 0 && bcrypt.compare(password, result[0].password)){
+            req.body.email = email;
+            res.send({result, msg: "you are signing in"})
+        }
+    })
+
 })
 
 
